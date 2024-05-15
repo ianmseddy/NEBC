7002c9e17f2ea2c2bbe2b0387b248657887ca1f5
ianmseddy <ianmseddy@gmail.com>
2024-05-15 23:13:41 +0000

    script updates

--- R/studyAreaFuns.R
+++ R/studyAreaFuns.R
@@ -6,8 +6,11 @@ setupSAandRTM <- function(destinationPath = "inputs", ecoprovinceNum = "14.1") {
                                  destinationPath = destinationPath,
                                  fun = "terra::vect")
   ep <- ep[ep$ECOPROVINC == ecoprovinceNum,]
-  RTM <- prepInputs(destinationPath = destinationPath,
-                    url = "https://drive.google.com/file/d/1g9jr0VrQxqxGjZ4ckF6ZkSMP-zuYzHQC/",
+  RTM <- reproducible::prepInputs(destinationPath = destinationPath,
+                                  url = paste0("https://ftp.maps.canada.ca/pub/nrcan_rncan/Forests_Foret/",
+                                               "canada-forests-attributes_attributs-forests-canada/",
+                                               "2001-attributes_attributs-2001/",
+                                               "NFI_MODIS250m_2001_kNN_Structure_Stand_Age_v1.tif"),
                     cropTo = ep, maskTo = ep,
                     filename2 = NULL,
                     method = c("near"), fun = "terra::rast")
@@ -17,9 +20,11 @@ setupSAandRTM <- function(destinationPath = "inputs", ecoprovinceNum = "14.1") {
 
 makeSppEquiv <- function() {
   speciesOfConcern = c("Pice_mar", "Pice_gla", "Popu_tre",
-                       "Pinu_con", "Betu_pap", "Betu_pap",
-                       "Pice_eng")
+                       "Pinu_con", "Betu_pap", "Pice_eng")
   sppEquiv = LandR::sppEquivalencies_CA[LandR %in% speciesOfConcern,]
-  sppEquiv$madeupFuel <- c("class3", "class3", "class2", "class4", "class3", "class2", "class3")
+  sppEquiv <- sppEquiv[LANDIS_traits != "PINU.CON.CON",] #drop shore pine
+  sppEquiv <- sppEquiv[order(sppEquiv$LandR)]
+                        # Betu_pap,  Pice_eng, Pice_gla, Pice_mar, Pinu_con, Popu_tre
+  sppEquiv$madeupFuel <- c("class2", "class3", "class3", "class3", "class4", "class2")
   return(sppEquiv)
 }
--- global.R
+++ global.R
@@ -7,10 +7,20 @@ getOrUpdatePkg <- function(p, minVer, repo) {
 
 getOrUpdatePkg("Require", "0.3.1.9015")
 getOrUpdatePkg("SpaDES.project", "0.0.8.9040")
-getOrUpdatePkg("LandR", "1.1.0.9079")
+# getOrUpdatePkg("LandR", "1.1.1")
+
+currentName <- "Taiga"
+if (currentName == "Taiga") {
+  ecoProvince <- c("4.3")
+  studyAreaPSPprov <- c("4.3", "12.3", "14.1", "9.1") #this is a weird combination
+
+} else {
+  ecoProvince <- "14.1"
+  studyAreaPSPprov <- c("14.1", "14.2", "14.3", "14.4")
+}
 
 
-out <- SpaDES.project::setupProject(
+inSim <- SpaDES.project::setupProject(
   updateRprofile = TRUE,
   Restart = TRUE,
   name = "NEBC",
@@ -34,39 +44,75 @@ out <- SpaDES.project::setupProject(
   ),
   times = list(start = 2011, end = 2021),
   params = list(
-    .globals = list(.studyAreaName = "NEBC",
+    .globals = list(.studyAreaName = currentName,
                     dataYear = 2011,
                     sppEquivCol = "LandR"),
-    gmcsDataPrep = list(PSPdataTypes = c("BC", "AB", "NFI"),
-                        doPlotting = TRUE
-                        ),
-    fireSense_SpreadFit = list(
-      # cores = rep("localhost", 30)
-      mode = "debug"
+    # gmcsDataPrep = list(PSPdataTypes = c("BC", "AB", "NFI"),
+    #                     doPlotting = TRUE
+    #                     ),
+    Biomass_borealDataPrep = list(
+      overrideAgeInFires = FALSE,
+      overrideBiomassinFires = FALSE
     ),
+    fireSense_SpreadFit = list(
+      cores = pemisc::makeIpsForNetworkCluster(
+        ipStart = "10.20.0",
+        ipEnd = c(97, 189, 220, 184, 106),
+        availableCores = c(28, 28, 28, 14, 14),
+        availableRAM = c(500, 500, 500, 250, 250),
+        localHostEndIp = 97,
+        proc = "cores",
+        nProcess = 10,
+        internalProcesses = 10,
+        sizeGbEachProcess = 1),
+      trace = 1, #cacheID_DE = "previous", Not a param?
+      mode = "fit", SNLL_FS_thresh = 5000,
+      doObjFunAssertions = FALSE),
     fireSense_dataPrepFit = list(
-      spreadFueLClass = "madeupFuel"
+      spreadFuelClassCol = "madeupFuel",
+      spreadFuelClassCol = "madeupFuel"
     )
   ),
+  climateVariablesForFire = list(ignition = "CMDsm",
+                                 spread = "CMDsm"),
   functions = "ianmseddy/NEBC@main/R/studyAreaFuns.R",
   sppEquiv = makeSppEquiv(),
-  studyArea = setupSAandRTM()$studyArea,
-  rasterToMatch = setupSAandRTM()$rasterToMatch,
-  studyAreaPSP = setupSAandRTM(ecoprovinceNum = c("14.1", "14.2", "14.3", "14.4"))$studyArea |>
+  studyArea = setupSAandRTM(ecoprovinceNum = ecoProvince)$studyArea,
+  rasterToMatch = setupSAandRTM(ecoprovinceNum = ecoProvince)$rasterToMatch,
+  rasterToMatchLarge = setupSAandRTM(ecoprovinceNum = ecoProvince)$rasterToMatch,
+  studyAreaLarge = setupSAandRTM(ecoprovinceNum = ecoProvince)$studyArea,
+  studyAreaPSP = setupSAandRTM(ecoprovinceNum = studyAreaPSPprov)$studyArea |>
     terra::aggregate() |>
     terra::buffer(width = 10000),
   packages = "googledrive",
-  require = c("PredictiveEcology/reproducible@modsForLargeArchives (HEAD)"),
+  # require = c("PredictiveEcology/reproducible@modsForLargeArchives (HEAD)"),
   useGit = TRUE
 )
 
-out$params$gmcsDataPrep$growthModel <- quote(nlme::lme(growth ~ logAge*(ATA + CMI),
-                                                       random = ~1 | OrigPlotID1,
-                                                       weights = varFunc(~plotSize^0.5 * periodLength),
-                                                       data = PSPmodelData))
-out$params$gmcsDataPrep$nullGrowthModel <- quote(nlme::lme(growth ~ logAge,
-                                                           random = ~1 | OrigPlotID1,
-                                                           weights = varFunc(~plotSize^0.5 * periodLength),
-                                                           data = PSPmodelData))
 
-inSim <- do.call(SpaDES.core::simInitAndSpades, out)
+#add this after because of the quoted functions
+inSim$climateVariables <- list(
+  historical_CMDsm = list(
+    vars = "historical_CMD_sm",
+    fun = quote(calcAsIs),
+    .dots = list(historical_years = 1991:2022)
+  ),
+  projected_CMDsm = list(
+    vars = "future_CMD_sm",
+    fun = quote(calcAsIs),
+    .dots = list(future_years = 2011:2100)
+  )
+)
+
+# out$params$gmcsDataPrep$growthModel <- quote(nlme::lme(growth ~ logAge*(ATA + CMI),
+#                                                        random = ~1 | OrigPlotID1,
+#                                                        weights = varFunc(~plotSize^0.5 * periodLength),
+#                                                        data = PSPmodelData))
+# out$params$gmcsDataPrep$nullGrowthModel <- quote(nlme::lme(growth ~ logAge,
+#                                                            random = ~1 | OrigPlotID1,
+#                                                            weights = varFunc(~plotSize^0.5 * periodLength),
+#                                                            data = PSPmodelData))
+googledrive::drive_auth("ianmseddy@gmail.com")
+outSim <- do.call(SpaDES.core::simInitAndSpades, inSim)
+
+saveSimList(outSim, paste0("outputs/outSim_", currentName, ".qs"))
