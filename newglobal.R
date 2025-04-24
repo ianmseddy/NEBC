9f0a1f981137943ccd5ddf4f2acae18cb4c14a84
Ian Eddy <ianmseddy@gmail.com>
2025-04-24 16:04:17 -0700

    many changes to submodules and global to run predict

--- newglobal.R
+++ newglobal.R
@@ -23,9 +23,11 @@ inSim <- SpaDES.project::setupProject(
                "PredictiveEcology/Biomass_borealDataPrep@development",
               # "PredictiveEcology/Biomass_core@development",
               # "PredictiveEcology/fireSense@development",
-              # "PredictiveEcology/fireSense_SpreadFit@lccFix",
-              "PredictiveEcology/fireSense_IgnitionFit@development"
-              # "PredictiveEcology/fireSense_EscapeFit@development"
+              # "PredictiveEcology/fireSense_SpreadFit@development",
+              "PredictiveEcology/fireSense_IgnitionFit@development",
+              "PredictiveEcology/fireSense_EscapeFit@development"
+              # "PredictiveEcology/fireSense_dataPrepPredict@development",
+              # "PredictiveEcology/fireSense@development"
   ),
   options = list(spades.allowInitDuringSimInit = TRUE,
                  spades.moduleCodeChecks = FALSE,
@@ -37,11 +39,11 @@ inSim <- SpaDES.project::setupProject(
                  reproducible.shapefileRead = "terra::vect",
                  spades.recoveryMode = 1
   ),
-  times = list(start = 2011, end = 2011),
+  times = list(start = 2011, end = 2021),
   loadOrder = unlist(modules), #load order must be passed or BBDP will be sourced prior to fsDPF.
   #also canClimateData must come before fireSense
-  climateVariablesForFire = list(ignition = c("CMDsp"),
-                                 spread = c("CMDsp")),
+  climateVariablesForFire = list(ignition = c("CMDsm"),
+                                 spread = c("CMDsm")),
   functions = "ianmseddy/NEBC@main/R/studyAreaFuns.R",
   #update mutuallyExlcusive Cols
   studyArea = {
@@ -78,19 +80,96 @@ inSim <- SpaDES.project::setupProject(
       .useCache = c(".inputObjects")
     ),
     fireSense_dataPrepFit = list(".studyAreaName" = paste0("FRU", FRU)),
-    canClimateData = list(".useCache" = c(".inputObjects", "init"))
+    canClimateData = list(".useCache" = c(".inputObjects"))
   )
 )
 
 
 inSim$climateVariables <- list(
-  historical_CMDsp = list(
-    vars = "historical_CMD_sp",
+  historical_CMDsm = list(
+    vars = "historical_CMD_sm",
     fun = quote(calcAsIs),
     .dots = list(historical_years = 1991:2022)
+  ),
+  projected_CMDsm = list(
+    vars = "future_CMD_sm",
+    fun = quote(calcAsIs),
+    .dots = list(future_years = 2011:2100)
   )
 )
 
-devtools::load_all("../fireSenseUtils")
-outSim <- SpaDES.core::simInitAndSpades2(inSim)
+pkgload::load_all("../fireSenseUtils")
 
+
+outSim <- SpaDES.core::simInitAndSpades2(inSim) |>
+  Cache()
+
+
+
+inSim2 <- SpaDES.project::setupProject(
+  require = "PredictiveEcology/scfmutils@development",
+  packages = c("usethis", "googledrive", "httr2", "RCurl", "XML"),
+  useGit=  TRUE,
+  paths = list(projectPath = "NEBC",
+               modulePath = file.path("modules"),
+               cachePath = file.path("cache"),
+               scratchPath = tempdir(),
+               inputPath = file.path("inputs"),
+               outputPath = file.path("outputs", paste0("FRU", FRU))
+  ),
+  modules = c( "PredictiveEcology/Biomass_speciesData@development",
+               "PredictiveEcology/Biomass_borealDataPrep@development",
+               "PredictiveEcology/Biomass_core@development",
+               "PredictiveEcology/fireSense_dataPrepPredict@development",
+               "PredictiveEcology/fireSense@development",
+               "PredictiveEcology/fireSense_IgnitionPredict@development",
+               "PredictiveEcology/fireSense_EscapePredict@master"
+  ),
+  options = list(spades.allowInitDuringSimInit = TRUE,
+                 spades.moduleCodeChecks = FALSE,
+                 useCache = TRUE,
+                 reproducible.inputPaths = "~/data",
+                 gargle_oauth_email = "ianmseddy@gmail.com",
+                 gargle_oauth_cache = "~/google_drive_cache",
+                 gargle_oauth_client_type = "web", #I think?
+                 reproducible.shapefileRead = "terra::vect",
+                 spades.recoveryMode = 1
+  ),
+  times = list(start = 2011, end = 2021),
+  loadOrder = unlist(modules), #load order must be passed or BBDP will be sourced prior to fsDPF.
+  #also canClimateData must come before fireSense
+  climateVariablesForFire = list(ignition = c("CMDsm"),
+                                 spread = c("CMDsm")),
+  functions = "ianmseddy/NEBC@main/R/studyAreaFuns.R",
+  #update mutuallyExlcusive Cols
+  studyArea = outSim$studyArea,
+  studyAreaLarge = outSim$studyAreaLarge,
+  rasterToMatch = outSim$rasterToMatch,
+  rasterToMatchLarge = outSim$rasterToMatchLarge,
+  projectedClimateRasters = outSim$projectedClimateRasters,
+  fireSense_IgnitionFitted = outSim$fireSense_IgnitionFitted,
+  nonForestedLCCGroups = outSim$nonForestedLCCGroups,
+  missingLCCgroup = outSim$missingLCCgroup,
+  ignitionFitRTM = outSim$ignitionFitRTM,
+  flammableRTM = outSim$flammableRTM2011,
+  rstLCC = outSim$rstLCC2011,
+  nonForest_timeSinceDisturbance = outSim$nonForest_timeSinceDisturbance2011,
+  sppEquiv = outSim$sppEquiv,
+  landcoverDT=  outSim$landcoverDT2011,
+  fireSense_IgnitionFitted = outSim$fireSense_IgnittionFitted,
+  fireSense_EscapeFitted = outSim$fireSense_EscapeFitted,
+  params = list(
+    .globals = list(.studyAreaName = paste0("FRU", FRU),
+                    dataYear = 2011,
+                    .plots = "png",
+                    sppEquivCol = "LandR"),
+    Biomass_borealDataPrep = list(
+      overrideAgeInFires = FALSE,
+      overrideBiomassInFires = FALSE,
+      .useCache = c(".inputObjects")
+    )
+  )
+)
+
+# pkgload::load_all("../SpaDES.core")
+outSim2 <- simInitAndSpades2(inSim2)
